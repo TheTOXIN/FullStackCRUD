1310f899c522a8739ecdc96c4720f0e06988ff06
toxin <yuri.belousov.1998@gmail.com>
2018-11-08 18:07:00 +0300

    back init

---
+++ fullback/src/main/java/com/toxin/fullback/FullbackApplication.java
@@ -0,0 +1,12 @@
+package com.toxin.fullback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullbackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FullbackApplication.class, args);
+    }
+}
