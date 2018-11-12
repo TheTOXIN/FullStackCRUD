775307bc03c4c8c3ee65ed4de8b6f9843443d992
toxin <yuri.belousov.1998@gmail.com>
2018-11-12 13:08:07 +0300

    back refactor

--- fullback/src/main/java/com/toxin/fullback/ContactController.java
+++ fullback/src/main/java/com/toxin/fullback/ContactController.java
@@ -3,7 +3,6 @@ package com.toxin.fullback;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.UUID;
 
 @RestController
 public class ContactController {
--- fullback/src/main/java/com/toxin/fullback/ContactRepository.java
+++ fullback/src/main/java/com/toxin/fullback/ContactRepository.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+
 @Repository
 public interface ContactRepository extends CrudRepository<Contact, String> {
 
